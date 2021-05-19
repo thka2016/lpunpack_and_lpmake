df1798926bd72a422d8269224e493b9c78228781
userr2020 <46326722+userr2019@users.noreply.github.com>
2021-05-19 12:42:47 +0800

    Update and rename make.sh to make_static.sh

--- make_static.sh
+++ make_static.sh
@@ -293,16 +293,14 @@ ar rcs liblp.a *.o
 
 cd ../partition_tools
 
-ar rcs libcrt0.a crt0.o
-
 if [ "$HOSTTYPE" = i686 ] || [ "$HOSTTYPE" = x86_64 ];then
-  clang++ -std=c++17 -I../include -o ../lpunpack -L. -lcrt0 lpunpack.cc ../liblp/liblp.a ../libsparse/libsparse.a -lz ../base/libbase.a ../liblog/liblog.a ../crypto/libcrypto.a ../libcrypto_utils/libcrypto_utils.a ../ext4_utils/libext4_utils.a -lpthread
+  clang++ -static -std=c++17 -I../include -o ../lpunpack -L. -lcrt0 lpunpack.cc ../liblp/liblp.a ../libsparse/libsparse.a -lz ../base/libbase.a ../liblog/liblog.a ../crypto/libcrypto.a ../libcrypto_utils/libcrypto_utils.a ../ext4_utils/libext4_utils.a -lpthread
 
-  clang++ -std=c++17 -I../include -o ../lpmake -L. -lcrt0 lpmake.cc ../liblp/liblp.a ../libsparse/libsparse.a -lz ../base/libbase.a ../liblog/liblog.a ../crypto/libcrypto.a ../libcrypto_utils/libcrypto_utils.a ../ext4_utils/libext4_utils.a -lpthread
+  clang++ -static -std=c++17 -I../include -o ../lpmake -L. -lcrt0 lpmake.cc ../liblp/liblp.a ../libsparse/libsparse.a -lz ../base/libbase.a ../liblog/liblog.a ../crypto/libcrypto.a ../libcrypto_utils/libcrypto_utils.a ../ext4_utils/libext4_utils.a -lpthread
 
-  clang++ -std=c++17 -I../include -o ../lpdump -L. -lcrt0 lpdump_host.cc lpdump.cc ../liblp/liblp.a ../libsparse/libsparse.a -lz ../base/libbase.a ../liblog/liblog.a ../crypto/libcrypto.a ../libcrypto_utils/libcrypto_utils.a ../ext4_utils/libext4_utils.a -lpthread
+  clang++ -static -std=c++17 -I../include -o ../lpdump -L. -lcrt0 lpdump_host.cc lpdump.cc ../liblp/liblp.a ../libsparse/libsparse.a -lz ../base/libbase.a ../liblog/liblog.a ../crypto/libcrypto.a ../libcrypto_utils/libcrypto_utils.a ../ext4_utils/libext4_utils.a -lpthread
 
-  clang++ -std=c++17 -I../include -o ../lpadd -L. -lcrt0 lpadd.cc ../liblp/liblp.a ../libsparse/libsparse.a -lz ../base/libbase.a ../liblog/liblog.a ../crypto/libcrypto.a ../libcrypto_utils/libcrypto_utils.a ../ext4_utils/libext4_utils.a -lpthread
+  clang++ -static -std=c++17 -I../include -o ../lpadd -L. -lcrt0 lpadd.cc ../liblp/liblp.a ../libsparse/libsparse.a -lz ../base/libbase.a ../liblog/liblog.a ../crypto/libcrypto.a ../libcrypto_utils/libcrypto_utils.a ../ext4_utils/libext4_utils.a -lpthread
 else
   clang++ -static -std=c++17 -I../include -o ../lpunpack lpunpack.cc ../liblp/liblp.a ../libsparse/libsparse.a ../zlib/libz.a ../base/libbase.a ../liblog/liblog.a ../crypto/libcrypto.a ../libcrypto_utils/libcrypto_utils.a ../ext4_utils/libext4_utils.a -lpthread
 
